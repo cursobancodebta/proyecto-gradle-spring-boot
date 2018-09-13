758dd79d3376b9fa9e90612dd7bd6119a211be70
Juan Pablo Ramirez <jpramirez@Juans-MacBook-Pro.local>
2018-09-12 22:37:44 -0500

    CORS

--- src/main/java/com/bancodebogota/fdsm/ejemplogradle/App.java
+++ src/main/java/com/bancodebogota/fdsm/ejemplogradle/App.java
@@ -5,7 +5,13 @@ package com.bancodebogota.fdsm.ejemplogradle;
  */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -15,4 +21,16 @@ public class App {
         SpringApplication.run(App.class, args);
     }
 
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers","Access-Control-Allow-Origin","Access-Control-Request-Method", "Access-Control-Request-Headers","Origin","Cache-Control", "Content-Type", "Authorization"));
+        configuration.setAllowedMethods(Arrays.asList("DELETE", "GET", "POST", "PATCH", "PUT"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
